ddba20f06a4db4350f95e2d1e9f14823d4219724
Sekgobela-Kevin <kevinnoko23@gmail.com>
2022-08-15 14:12:49 +0200

    Update: analyse.py, exceptions.py
    exceptions.py has been updated with three(3) ore exception classes for parts
    of student numbers. .create_error_message() method of exception classes have
    ben updated to no longer accept explicit argumnets but get its data through
    attributes of its objects.
    
    analyse.py was updated to now use new exceptions and was new functions were added
    for dealing with parts of student number. The functions were also used on existing
    functions for reusability.

--- source/ulsid/analyse.py
+++ source/ulsid/analyse.py
@@ -26,7 +26,41 @@ def calculate_position_length(
     # starting at year_first_position.
     difference = (year_capacity - year_first_position) - 1
     return len(str(difference))
+    
+def position_part_to_position(position_part: str, *args, **kwargs):
+    # Convert position part to position integer
+    position_length = calculate_position_length(*args, **kwargs)
+    if len(position_part) == position_length:
+        return int(position_part)
+    else:
+        raise exceptions.PositionPartError(position_part)
 
+def position_to_position_part(position: int, *args, **kwargs):
+    # Convert position integer to its position part
+    position_length = calculate_position_length(*args, **kwargs)
+    return str(position).zfill(position_length)
+
+def year_part_to_year(year_part: str, strict=True):
+    # Convert position integer to its position part
+    if strict and len(year_part) == 2:
+        return int("19"+year_part)
+    elif len(year_part) >= 4:
+        return int(year_part)
+    else:
+        raise exceptions.YearPartError(year_part)
+
+def year_to_year_part(year: int, strict=True):
+    # Convert year integer to its year part
+    year_str = str(year)
+    if len(year_str) >= 4:
+        if strict and year_str.startswith("19"):
+            return year_str[2:]
+        return year_str
+    else:
+        err_msg = "Year {year} should atleast contain 4 digits"
+        raise ValueError(err_msg.format(year=year))
+    
+        
 def extract_position_part(student_number: int, *args, **kwargs):
     # Extracts position part of student number
     student_number: str = str(student_number)
@@ -36,7 +70,7 @@ def extract_position_part(student_number: int, *args, **kwargs):
     if len(position_part) == position_length:
         return position_part
     else:
-        raise exceptions.PositionPartError(student_number)
+        raise exceptions.PositionPartError(position_part, student_number)
 
 def extract_year_part(student_number: int, *args, **kwargs):
     # Extracts year part of student number
@@ -47,24 +81,27 @@ def extract_year_part(student_number: int, *args, **kwargs):
     if len(year_part) == 2 or len(year_part) >= 4:
         return year_part
     else:
-        raise exceptions.YearPartError(student_number)
+        # Year of student number should contain 2 or 4+ chars.
+        # Years with 0, 1 or 3 are considered invalid.
+        raise exceptions.YearPartError(year_part, student_number)
 
 def extract_position(student_number: int, *args, **kwargs):
     # Extracts posetion part of student number
     position_part = extract_position_part(student_number, *args, **kwargs)
-    return int(position_part)
+    try:
+        return position_part_to_position(position_part, *args, **kwargs)
+    except exceptions.PositionPartError:
+        raise exceptions.PositionPartError(position_part, student_number)
 
 def extract_year(student_number: int, *args, **kwargs):
     # Extracts year of student number
     year_part = extract_year_part(student_number, *args, **kwargs)
-    if len(year_part) == 2:
-        return int("19"+year_part)
-    elif len(year_part) >= 4:
-        return int(year_part)
-    else:
-        # Year of student number should contain 2 or 4+ chars.
-        # Years with 0, 1 or 3 are considered invalid.
-        raise exceptions.InvalidStudentNumber(student_number)
+    try:
+        return year_part_to_year(year_part, *args, **kwargs)
+    except exceptions.YearPartError:
+        # Re-raise exception with student number included
+        raise exceptions.YearPartError(year_part, student_number)
+
 
 def split_student_number(student_number: int, *args, **kwargs):
     # Splits student number into 2 parts representing year and 
@@ -72,8 +109,18 @@ def split_student_number(student_number: int, *args, **kwargs):
     position_part = extract_position_part(student_number, *args, **kwargs)
     year_part = extract_year_part(student_number, *args, **kwargs)
     return year_part, position_part
-    
+
+def student_number_valid(student_number: int, *args, **kwargs):
+    # Checks if student number is valid
+    try:
+        # If student number can be split then its valid
+        split_student_number(student_number, *args, **kwargs)
+    except exceptions.StudentNumberError:
+        return False
+    else:
+        return True
+
 
 if __name__ == "__main__":
-    print(split_student_number(5290, year_capacity=100))
+    print(student_number_valid(str(8529216)))
 
--- source/ulsid/exceptions.py
+++ source/ulsid/exceptions.py
@@ -5,6 +5,7 @@ Author: Sekgobela Kevin
 Date: August 2022
 Language: Python 3
 '''
+import typing
 
 class UlidError(Exception):
     '''Base class for ulid exceptions'''
@@ -13,34 +14,53 @@ class UlidError(Exception):
 class StudentNumberError(UlidError):
     '''Base class for exceptions regarding student numbers'''
     def __init__(self, student_number: int) -> None:
-        error_msg = self.create_error_message(student_number)
+        self.student_number = student_number
+        error_msg = self.create_error_message()
         super().__init__(error_msg)
 
-    def create_error_message(self, student_number: int):
+    def create_error_message(self):
         err_msg = "There is problem with student number {student_id}"
-        return err_msg.format(student_id=student_number)
+        return err_msg.format(student_id=self.student_number)
 
 class InvalidStudentNumber(StudentNumberError):
     '''Student number is invalid'''
-    def create_error_message(self, student_number: int):
+    def create_error_message(self):
         err_msg = "Student number {student_id} is invalid"
-        return err_msg.format(student_id=student_number)
+        return err_msg.format(student_id=self.student_number)
 
 class UnsupportedStudentNumber(StudentNumberError):
     '''Student number is valid but not supported'''
-    def create_error_message(self, student_number: int):
+    def create_error_message(self):
         err_msg = "Student number {student_id} is not supported"
-        return err_msg.format(student_id=student_number)
+        return err_msg.format(student_id=self.student_number)
 
 
-class YearPartError(StudentNumberError):
+class StudentNumberPartError(StudentNumberError):
+    '''Exceptions regarding parts of student number'''
+    part_name = "Part"
+    
+    def __init__(self, part: str, student_number:int=None) -> None:
+        self.part = part
+        super().__init__(student_number)
+
+    def create_error_message(self):
+        if self.student_number == None:
+            student_number = ""
+        else:
+            student_number = self.student_number
+        err_msg = "{part_name} {part} of student number " +\
+            "{student_id} is invalid"
+        return err_msg.format(
+            part=self.part, 
+            student_id=student_number, 
+            part_name=self.part_name
+        )
+
+
+class YearPartError(StudentNumberPartError):
     '''Exception regarding year part of student number'''
-    def create_error_message(self, student_number: int):
-        err_msg = "Year part of student number {student_id} is invalid"
-        return err_msg.format(student_id=student_number)
+    part_name = "Year part"
 
-class PositionPartError(StudentNumberError):
+class PositionPartError(StudentNumberPartError):
     '''Exception regarding position part of student number'''
-    def create_error_message(self, student_number: int):
-        err_msg = "Position part of student number {student_id} is invalid"
-        return err_msg.format(student_id=student_number)
+    part_name = "Posetion part"
