78b4e6c68c1994453327376058adbd3c2f96779a
Sekgobela-Kevin <kevinnoko23@gmail.com>
2022-08-16 15:51:54 +0200

    Updtes & Improvements: analyse.py, generate.py
    This commit fixes bugs introduced from previous commit.
    
    Changes
    -------
    analyse.py - position_to_position_part() function updated to now raise exceptions.
               - Global constants were renamed and made public.
               - calculate_position_length() function updated to use (+) than (-).
    
    generate.py - guess_position() and guess_year() updated to raise exceptions and accept options
                  keywords argumnets.
                - Calculation of start_pos and end_pos argumnets of guess_position() were updated to take into
                  account provided year capacity and year fisr position.
                - guess_year() now takes into consideration 'strict' argument and would raise exception if
                  it is violated.
    
    Issues
    ------
    - guess_year() does not raise exception when start year is less than default start year(currently 1959).
      It should be strict that student numbers only be valid only when they are after default start year(1959).

--- source/ulsid/analyse.py
+++ source/ulsid/analyse.py
@@ -9,112 +9,149 @@ import typing
 from . import exceptions
 
 
-_YEAR_CAPACITY = 100000
-_YEAR_FIRST_POSITION = 0
+DEFAULT_YEAR_FIRST_POS = 0 # First position likely 0
+DEFAULT_YEAR_CAPACITY = 100000 # Max capacity for a each year
+
+
+def caculate_last_postion(
+    year_first_position: typing.Union[int, None] = None, 
+    year_capacity: typing.Union[int, None] = None):
+    # Calculates last position for student numbers
+    # year_first_position: first position for the year.
+    # year_capacity: maximum student that can be accomodated in a year.
+    if year_first_position ==  None:
+        year_first_position = DEFAULT_YEAR_FIRST_POS
+    if year_capacity == None:
+        year_capacity = DEFAULT_YEAR_CAPACITY
+    return (year_capacity + year_first_position) - 1
 
 def calculate_position_length(
     year_first_position: typing.Union[int, None] = None, 
     year_capacity: typing.Union[int, None] = None):
     # Calculates length(chars) position part of student number.
-    # year_first_position: first position for the year.
-    # year_capacity: maximum student that can be accomodated in a year.
-    if year_first_position ==  None:
-        year_first_position = _YEAR_FIRST_POSITION
-    if year_capacity == None:
-        year_capacity = _YEAR_CAPACITY
-    # Calculates length of digits that can handle capacity of students
-    # starting at year_first_position.
-    difference = (year_capacity - year_first_position) - 1
-    return len(str(difference))
+    last_position = caculate_last_postion(
+        year_first_position, year_capacity
+    )
+    return len(str(last_position))
+
+
+def year_part_valid(year_part: str, strict=True):
+    # Checks if year part is valid
+    if strict and len(year_part) == 2:
+        return True
+    elif len(year_part) >= 4:
+        return True
+    else:
+        return False
+
+def year_valid(year: int):
+    # Checks if year is valid
+    return len(str(year)) >= 4
+
+
+def position_valid(position: int, **kwargs):
+    # Check if position is valid
+    first_position = kwargs.get("year_first_position", 
+        DEFAULT_YEAR_FIRST_POS)
+    capacity = kwargs.get("year_capacity", 
+        DEFAULT_YEAR_CAPACITY)
+    last_postion = caculate_last_postion(first_position, capacity)
+    return position >= first_position and position <= last_postion
     
-def position_part_to_position(position_part: str, *args, **kwargs):
+def position_part_valid(position_part: str, **kwargs):
+    # Checks if position part is valid
+    return position_valid(int(position_part), **kwargs)
+    
+def position_part_to_position(position_part: str, **kwargs):
     # Convert position part to position integer
-    position_length = calculate_position_length(*args, **kwargs)
-    if len(position_part) == position_length:
+    if position_part_valid(position_part, **kwargs):
         return int(position_part)
     else:
         raise exceptions.PositionPartError(position_part)
 
-def position_to_position_part(position: int, *args, **kwargs):
+def position_to_position_part(position: int, **kwargs):
     # Convert position integer to its position part
-    position_length = calculate_position_length(*args, **kwargs)
-    return str(position).zfill(position_length)
+    if position_valid(position, **kwargs):
+        position_length = calculate_position_length(**kwargs)
+        return str(position).zfill(position_length)
+    else:
+        raise exceptions.InvalidPositionError(position)
 
 def year_part_to_year(year_part: str, strict=True):
     # Convert position integer to its position part
-    if strict and len(year_part) == 2:
-        return int("19"+year_part)
-    elif len(year_part) >= 4:
-        return int(year_part)
+    if year_part_valid(year_part, strict):
+        if strict and len(year_part) == 2:
+            return int("19"+year_part)
+        else:
+            return int(year_part)
     else:
         raise exceptions.YearPartError(year_part)
 
 def year_to_year_part(year: int, strict=True):
     # Convert year integer to its year part
     year_str = str(year)
-    if len(year_str) >= 4:
+    if year_valid(year):
         if strict and year_str.startswith("19"):
             return year_str[2:]
         return year_str
     else:
-        err_msg = "Year {year} should atleast contain 4 digits"
-        raise ValueError(err_msg.format(year=year))
+        raise exceptions.InvalidYearError(year)
     
         
-def extract_position_part(student_number: int, *args, **kwargs):
+def extract_position_part(student_number: int, **kwargs):
     # Extracts position part of student number
     student_number: str = str(student_number)
-    position_length = calculate_position_length(*args, **kwargs)
+    position_length = calculate_position_length(**kwargs)
     position_part = student_number[position_length*-1:]
     # Position part should have same digits length as position_length
-    if len(position_part) == position_length:
+    if position_part_valid(position_part, **kwargs):
         return position_part
     else:
         raise exceptions.PositionPartError(position_part, student_number)
 
-def extract_year_part(student_number: int, *args, **kwargs):
+def extract_year_part(student_number: int, strict=True, **kwargs):
     # Extracts year part of student number
     student_number: str = str(student_number)
-    position_length = calculate_position_length(*args, **kwargs)
+    position_length = calculate_position_length(**kwargs)
     year_part = student_number[:position_length*-1]
     # Year part should be atleast 2 or 4+ digits
-    if len(year_part) == 2 or len(year_part) >= 4:
+    if year_part_valid(year_part, strict):
         return year_part
     else:
         # Year of student number should contain 2 or 4+ chars.
         # Years with 0, 1 or 3 are considered invalid.
         raise exceptions.YearPartError(year_part, student_number)
 
-def extract_position(student_number: int, *args, **kwargs):
+def extract_position(student_number: int, **kwargs):
     # Extracts posetion part of student number
-    position_part = extract_position_part(student_number, *args, **kwargs)
+    position_part = extract_position_part(student_number, **kwargs)
     try:
-        return position_part_to_position(position_part, *args, **kwargs)
+        return position_part_to_position(position_part, **kwargs)
     except exceptions.PositionPartError:
         raise exceptions.PositionPartError(position_part, student_number)
 
-def extract_year(student_number: int, *args, **kwargs):
+def extract_year(student_number: int, strict=True, **kwargs):
     # Extracts year of student number
-    year_part = extract_year_part(student_number, *args, **kwargs)
+    year_part = extract_year_part(student_number, **kwargs)
     try:
-        return year_part_to_year(year_part, *args, **kwargs)
+        return year_part_to_year(year_part, strict)
     except exceptions.YearPartError:
         # Re-raise exception with student number included
         raise exceptions.YearPartError(year_part, student_number)
 
 
-def split_student_number(student_number: int, *args, **kwargs):
+def split_student_number(student_number: int, strict=True, **kwargs):
     # Splits student number into 2 parts representing year and 
     # application position
-    position_part = extract_position_part(student_number, *args, **kwargs)
-    year_part = extract_year_part(student_number, *args, **kwargs)
+    position_part = extract_position_part(student_number, **kwargs)
+    year_part = extract_year_part(student_number, strict)
     return year_part, position_part
 
-def student_number_valid(student_number: int, *args, **kwargs):
+def student_number_valid(student_number: int, strict, **kwargs):
     # Checks if student number is valid
     try:
         # If student number can be split then its valid
-        split_student_number(student_number, *args, **kwargs)
+        split_student_number(student_number, strict, **kwargs)
     except exceptions.StudentNumberError:
         return False
     else:
@@ -122,5 +159,5 @@ def student_number_valid(student_number: int, *args, **kwargs):
 
 
 if __name__ == "__main__":
-    print(student_number_valid(str(8529216)))
+    print(split_student_number(str(8534229216),  strict=False))
 
--- source/ulsid/generate.py
+++ source/ulsid/generate.py
@@ -5,53 +5,98 @@ Author: Sekgobela Kevin
 Date: August 2022
 Language: Python 3
 '''
+from . import exceptions
 from . import analyse
+
 import random
 import time
 
 
 # Year UL started to operate
-START_YEAR = 1959
-
+DEFAULT_START_YEAR = 1959
 # Current year
-CURRENT_YEAR = time.gmtime(time.time()).tm_year
+DEFAULT_END_YEAR = time.gmtime(time.time()).tm_year + 1
 
 
-def guess_position(start_pos:int=None, end_pos:int=None):
+
+def guess_position(start_pos:int=None, end_pos:int=None, **kwargs):
     # Guesses position to be used with student number
+
+    # Gets year capacity from arguments else gets default one
+    year_capacity = kwargs.get("year_capacity", 
+        analyse.DEFAULT_YEAR_CAPACITY)
+    # Gets first position from arguments else gets default one
+    first_position = kwargs.get("year_first_position", 
+        analyse.DEFAULT_YEAR_FIRST_POS)
+
+    # Updates start_pos if not provided on argumnets
     if start_pos == None:
-        start_pos = analyse._YEAR_FIRST_POSITION
+        start_pos = first_position
+    
+    # Calculates max position based on start pos and capacity
+    max_end_pos = analyse.caculate_last_postion(start_pos, year_capacity)
+    
+    # Updates end_pos if not provided on argumnets
     if end_pos == None:
-        end_pos = analyse._YEAR_CAPACITY
-    return random.randrange(start_pos, end_pos)
+        end_pos = max_end_pos
+
+    # Raises exceptions if values dont match
+    # Its important to raise exceptions to help find bugs.
+    if start_pos > end_pos:
+        err_msg = "Start position {} cant be less than end position {}"
+        error_msg = err_msg.format(start_pos, end_pos)
+
+    if start_pos < first_position:
+        err_msg = "Start position {} cant be less than first position {}"
+        error_msg = err_msg.format(start_pos, first_position)
+        raise ValueError(error_msg)
+
+    if end_pos > max_end_pos:
+        err_msg = "End position {} cant be greater than max position {}"
+        err_msg = err_msg.format(end_pos, max_end_pos)
+        raise ValueError(err_msg)
+    
+    # Uses ranom of start pos and end pos to guess position
+    return random.randint(start_pos, end_pos)
 
 def guess_position_part(start_pos:int=None, end_pos:int=None, **kwargs):
     # Guesses position part to be used with student number
-    position = guess_position(start_pos, end_pos)
+    position = guess_position(start_pos, end_pos, **kwargs)
     return analyse.position_to_position_part(position, **kwargs)
 
 
-def guess_year(start_year:int=None, end_year:int=None):
+def guess_year(start_year:int=None, end_year:int=None, strict=True):
     # Guesses year to be used with student number
     if start_year == None:
-        start_year = START_YEAR
+        start_year = DEFAULT_START_YEAR
     if end_year == None:
-        end_year = CURRENT_YEAR + 1
-    return random.randrange(start_year, end_year)
+        end_year = DEFAULT_END_YEAR
+
+    if start_year > end_year:
+        err_msg = "End year {} cant be greater than end year {}"
+        raise ValueError(err_msg.format(start_year, end_year))
+    
+    if strict and end_year > DEFAULT_END_YEAR:
+        err_msg = "End year {} cant be greater than default end year " +\
+            "{} when strict is enabled."
+        raise ValueError(err_msg.format(end_year, DEFAULT_END_YEAR))
+
+    return random.randint(start_year, end_year)
 
 def guess_year_part(start_year:int=None, end_year:int=None, strict=True):
     # Guesses year part to be used with student number
-    year = guess_year(start_year, end_year)
+    year = guess_year(start_year, end_year, strict)
     return analyse.year_to_year_part(year, strict)
 
 
 def guess_student_number(
     start_year:int=None, 
     end_year:int=None, 
-    strict=True, 
     start_pos:int=None, 
     end_pos:int=None, 
+    strict=True,
     **kwargs):
+    # Guesses student number limited by provided arguments
     year_part = guess_year_part(start_year, end_year, strict)
     position_part = guess_position_part(start_pos, end_pos, **kwargs)
     return year_part + position_part
@@ -60,4 +105,4 @@ def guess_student_number(
 
 if __name__ == "__main__":
     student_number = 202264623
-    print(guess_student_number(strict=False, end_pos=1000,year_capacity=40))
+    print(guess_student_number(strict=False, end_year=3013))
